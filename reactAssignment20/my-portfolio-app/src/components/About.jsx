afe5eabdbc02443f3964362e72aae6d76e9f15cc
Tuba Muktadir <muktadir1090@gmail.com>
2024-10-05 17:03:20 -0400

    update file

--- reactAssignment20/my-portfolio-app/src/components/About.jsx
+++ reactAssignment20/my-portfolio-app/src/components/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import profile from '../assets/image/profilepic.png'
 
 function About() {
   return (
     <section id="about">
-      <img src="profilepic.png" alt="Developer" />
+      <img height ="300px" src={profile} alt="Developer" />
       <p>I am a passionate and dedicated Software Engineer with a dual specialization in Web Development and Software Development in Testing (SDET). Over the past 8 years, I’ve honed my craft in both development and testing, bringing a comprehensive and well-rounded skill set to every project I work on. With 6 years of experience as an SDET and 2 years as a web developer, I have cultivated a deep technical expertise in building scalable, efficient, and robust web applications while ensuring their reliability through rigorous automated testing frameworks.
 
 My experience spans across various industries, including projects for Fortune 500 companies, where I’ve been entrusted with high-impact, mission-critical tasks that demand precision and scalability. This has provided me with invaluable exposure to large-scale, complex systems that operate in high-demand environments, pushing me to design and develop solutions that not only meet immediate business needs but also anticipate future growth.
--- reactAssignment20/my-portfolio-app/src/components/Resume.jsx
+++ reactAssignment20/my-portfolio-app/src/components/Resume.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import resume from '../assets/resume.pdf'
 
 function Resume() {
   return (
     <section id="resume">
-      <a href="resume.pdf" download>Download Resume</a>
+      <a href={resume} download>Download Resume</a>
       <ul>
-        <li>Proficiency 1</li>
-        <li>Proficiency 2</li>
+        <li>Web Developer</li>
+        <li>Software Development in Test</li>
         {/* Add more proficiencies */}
       </ul>
     </section>
